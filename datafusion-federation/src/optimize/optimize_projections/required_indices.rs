43634abb02514d27c6f4db79bc266dfc8f0a5071
sgrebnov <sergei.grebnov@gmail.com>
2024-11-28 20:38:28 -0800

    Add original license headers

--- datafusion-federation/src/optimize/optimize_projections/required_indices.rs
+++ datafusion-federation/src/optimize/optimize_projections/required_indices.rs
@@ -1,3 +1,22 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+/// Copy of DataFusion's [`RequiredIndicies`](https://github.com/apache/datafusion/blob/main/datafusion/optimizer/src/optimize_projections/required_indices.rs) implementation.
+
 use std::result;
 
 use datafusion::{common::{tree_node::TreeNodeRecursion, Column, DFSchemaRef}, error::DataFusionError, logical_expr::LogicalPlan, prelude::Expr};
@@ -5,8 +24,6 @@ use datafusion::{common::{tree_node::TreeNodeRecursion, Column, DFSchemaRef}, er
 use super::outer_columns;
 
 type Result<T, E = DataFusionError> = result::Result<T, E>;
-
-/// Copy of DataFusion's [`RequiredIndicies`](https://github.com/apache/datafusion/blob/main/datafusion/optimizer/src/optimize_projections/required_indices.rs) implementation.
 
 /// Represents columns in a schema which are required (used) by a plan node
 ///
